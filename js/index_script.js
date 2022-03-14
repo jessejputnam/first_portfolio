98573cc28237c5e90ee4458ccb7e9a339d9d2a54
Jesse Putnam <jessejputnam@gmail.com>
2022-03-14 14:37:28 -0400

    Fix smooth scrolling for iOS and Safari

--- js/index_script.js
+++ js/index_script.js
@@ -8,10 +8,18 @@ TABLE OF CONTENTS:
 
 /* ##################### #DOM Variables ######################### */
 const btnAboutMe = document.querySelectorAll(".btn__aboutme");
+const aboutMeSection = document.getElementById("about-me");
 
 /* ##################### #Buttons ######################### */
 btnAboutMe.forEach((btn) => {
   btn.addEventListener("click", () => {
-    document.getElementById("about-me").scrollIntoView({ behavior: "smooth" });
+    const aboutMeCoords = aboutMeSection.getBoundingClientRect();
+    window.scrollTo({
+      left: aboutMeCoords.left + window.pageXOffset,
+      top: aboutMeCoords.top + window.pageYOffset,
+      behavior: "smooth",
+    });
+    // Does not work for iOS and IE and Safari
+    // aboutMeSection.scrollIntoView({ behavior: "smooth" });
   });
 });
