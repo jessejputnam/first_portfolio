b391ca6f2240fca4311e81f06cce27f3f089f3f1
Jesse Putnam <jessejputnam@gmail.com>
2022-03-16 08:29:46 -0400

    Add section slide in / reveal (JS)

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ const mobileMenuToggle = document.querySelector(
   ".header__mobile__links__toggle"
 );
 const mobileModalLinks = document.querySelector(".header__mobile-links__modal");
+const allSections = document.querySelectorAll(".section");
 
 /* ################### #Mobile Menu Modal ###################### */
 mobileMenuToggle.addEventListener("click", () => {
@@ -22,4 +23,24 @@ mobileMenuToggle.addEventListener("click", () => {
     mobileMenuToggle.src = "images/icons/hamburger.svg";
     mobileMenuToggle.style.marginRight = "0";
   }
+});
+
+// Reveal Sections on scroll
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
 });
